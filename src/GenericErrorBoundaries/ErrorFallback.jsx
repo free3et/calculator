73f41a23998adb32e46849eae3d906e6eec14b55
free3et <free3et@gmail.com>
2022-12-02 17:51:12 +0200

    add className

--- src/GenericErrorBoundaries/ErrorFallback.jsx
+++ src/GenericErrorBoundaries/ErrorFallback.jsx
@@ -1,6 +1,6 @@
 function ErrorFallback({ error, resetErrorBoundary }) {
   return (
-    <div role="alert">
+    <div className="error">
       <p>Something went wrong:</p>
       <h3>{error.message}</h3>
       <button onClick={resetErrorBoundary}>Try again</button>
